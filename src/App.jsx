1154823ec33ca04d11e01ead5f0c18a027a34a9c
Izam Ghali <izamghali18@gmail.com>
2023-12-13 21:12:09 +0700

    UPDATED: custom colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-violet-600 text-white text-4xl'>Hello World!</h1>
+      <h1 className='bg-black text-darkCompletedGreen text-4xl'>Hello World!</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkCompletedGreen': '#A5F39E',
+        'darkProgressYellow': '#F2D191',
+        'darkTodoRed': '#F285C9',
+        'darkFinishBlue': '#B6CFF2',
+        'lightCompletedGreen': '#55E148',
+        'lightProgressYellow': '#F2D191',
+        'lightTodoRed': '#F285C9',
+        'lightFinishBlue': '#87B3F0'
+      }
+    },
   },
   plugins: [],
 }
